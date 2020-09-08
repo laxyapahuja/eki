07f0e77ef3a4b7fde99bd6d294f039c624be7aef
Laxya Pahuja <31318248+laxyapahuja@users.noreply.github.com>
2020-09-08 15:57:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="eki", # Replace with your own username
-    version="0.0.5",
+    name="eki",
+    version="0.0.6",
     author="Laxya Pahuja",
-    author_email="mail@laxya.co",
+    author_email="laxya.pahuja8@gmail.com",
     description="MyAnimeList tracker for local anime files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,10 +18,15 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={'console_scripts': ['eki=eki.__main__:main']}
     python_requires='>=3.6',
     install_requires=[
         'malupdate',
         'pywin32',
         'pymediainfo'
     ],
+    project_urls={
+        'Documentation': 'https://github.com/laxyapahuja/eki/README.md',
+        'Source': 'https://github.com/laxyapahuja/eki'
+    },
 )
